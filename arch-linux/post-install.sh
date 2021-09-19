3aebd54ea5d8a962ddef6b00d96890652e83e191
yavorski <yavorski.wrx@gmail.com>
2021-09-19 17:35:15 +0300

    arch reflector options

--- arch-linux/post-install.sh
+++ arch-linux/post-install.sh
@@ -32,7 +32,24 @@ yes | pacman -S alacritty kitty
 yes | pacman -S xorg-xdpyinfo xorg-xprop xorg-xrandr xorg-xwininfo
 yes | pacman -S neofetch catimg chafa feh imagemagick jp2a libcaca nitrogen
 
-# @todo update service
+# reflector
 yes | pacman -Syu reflector
+
+# setup reflector options
+REFLECTOR_CONF="/etc/xdg/reflector/reflector.conf"
+mv $REFLECTOR_CONF "$REFLECTOR_CONF.BAK"
+touch $REFLECTOR_CONF
+cat >> $REFLECTOR_CONF << EOL
+# $REFLECTOR_CONF
+# ------------------------------------------
+--age 24
+--latest 128
+--protocol https
+--sort rate
+--sort score
+--sort country
+--country 'BG,RO,HU,SI,CZ,NL,PL,UA,CH,DE,FR,IT,FR,DK,LT,LV,EE,FI,NO,SE,GB,PT,RU,CA,US,*,CN'
+--save /etc/pacman.d/mirrorlist
+EOL
 
 echo @finish post-install script!
