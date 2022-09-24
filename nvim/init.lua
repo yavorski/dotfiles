882bdecda3f15d0cfabce9f1a66f724e71ec6916
yavorski <yavorski.wrx@gmail.com>
2022-09-24 23:03:37 +0300

    nvim - tabline - settings change

--- nvim/init.lua
+++ nvim/init.lua
@@ -218,8 +218,8 @@ packer.startup(function()
           section_separators = { '', '' }, -- disable separators
           component_separators = { '', '' }, -- disable separators
           max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
-          show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
-          show_devicons = true, -- this shows devicons in buffer section
+          show_tabs_always = true, -- this shows tabs only when there are more than one tab or if the first tab is named
+          show_devicons = false, -- this shows devicons in buffer section
           show_bufnr = false, -- this appends [bufnr] to buffer section -- buffer number
           show_filename_only = false, -- shows base filename only instead of relative path in filename
           modified_icon = "^ ", -- change the default modified icon
