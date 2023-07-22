3c39b03e7515ac0d61e934dc15a24a2b60110b55
yavorski <yavorski.wrx@gmail.com>
2023-07-22 16:13:43 +0300

    nvim cmp add key maps

--- nvim/init.lua
+++ nvim/init.lua
@@ -1406,6 +1406,8 @@ AutoComplete.setup = function()
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
       ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<A-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<A-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
       ["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
       ["<S-CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
       ["<Tab>"] = cmp.mapping(AutoComplete.select_next_suggestion, { "i", "s" }),
