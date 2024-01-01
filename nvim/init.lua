e808fa97e0bf51950166f670e85e3de3c20e9d67
yavorski <yavorski.wrx@gmail.com>
2024-01-01 15:46:58 +0200

    nvim mini hipatterns

--- nvim/init.lua
+++ nvim/init.lua
@@ -585,11 +585,14 @@ Lazy.use {
 -- highlight patterns / display #rrggbb colors
 Lazy.use {
   "echasnovski/mini.hipatterns",
-  ft = { "lua", "conf", "css", "scss", "stylus", "html", "yaml", "markdown" },
+  ft = { "lua", "conf", "css", "scss", "stylus", "html", "toml", "yml", "yaml", "markdown" },
   config = function()
-    local patterns = require("mini.hipatterns")
-    local hex_color = patterns.gen_highlighter.hex_color()
-    patterns.setup({ highlighters = { hex_color } })
+    local hipatterns = require("mini.hipatterns")
+    hipatterns.setup({
+      highlighters = {
+        hex_color = hipatterns.gen_highlighter.hex_color()
+      }
+    })
   end
 }
 
