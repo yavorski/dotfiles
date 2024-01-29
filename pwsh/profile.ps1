e495f5c29a6a06c247d6dad6daf02a3ddefe44eb
yavorski <yavorski.wrx@gmail.com>
2024-01-29 11:03:35 +0200

    pwsh profile

---
+++ pwsh/profile.ps1
@@ -0,0 +1,76 @@
+# ---------------------------------------------------------------------
+# C:\Users\<User>\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+# ---------------------------------------------------------------------
+
+If (!$PROFILE.Contains("NuGet_profile")) {
+	# powershell terminal
+	Import-Module posh-git
+	Import-Module PSReadLine
+	Invoke-Expression (&starship init powershell)
+
+	Set-PSReadLineOption -PredictionSource History
+	Set-PSReadLineOption -PredictionViewStyle ListView
+
+	# Set-PSReadLineOption -EditMode Vi
+	Set-PSReadLineOption -EditMode Windows
+	Set-PSReadLineKeyHandler -Chord ctrl+p -Function PreviousHistory
+	Set-PSReadLineKeyHandler -Chord ctrl+n -Function NextHistory
+
+	# PowerShell parameter completion shim for the winget CLI
+	Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+		param($wordToComplete, $commandAst, $cursorPosition)
+			[Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+			$Local:word = $wordToComplete.Replace('"', '""')
+			$Local:ast = $commandAst.ToString().Replace('"', '""')
+			winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+				[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+			}
+	}
+
+	# PowerShell parameter completion shim for the dotnet CLI
+	Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+		param($commandName, $wordToComplete, $cursorPosition)
+		dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+			[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+		}
+	}
+
+	Set-Alias -Name g -Value git
+	function GitStatus { git status }
+	Set-Alias -Name gs -Value GitStatus
+
+	Set-Location -Path C:\dev
+}
+Else {
+	# Visual Studio - Package Manager Console
+}
+
+
+# ----------------------------------------------------------------------------------------------
+# This profile for powershell core is OK with neovim lsp powershell
+# CurrentUserCurrentHost: C:\Users\<User>\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+# ----------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------
+# Profile Location for powershell 5
+# C:\Users\<User>\Documents\WindowsPowerShell\profile.ps1
+# ----------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------
+# Profile Location for pwsh core
+# ----------------------------------------------------------------------------------------------
+# > echo $PROFILE
+# > $PROFILE | Select-Object *Host* | Format-List
+# AllUsersAllHosts: C:\Program Files\PowerShell\7\profile.ps1
+# AllUsersCurrentHost: C:\Program Files\PowerShell\7\Microsoft.PowerShell_profile.ps1
+# CurrentUserAllHosts: C:\Users\<User>\Documents\PowerShell\profile.ps1
+# CurrentUserCurrentHost: C:\Users\<User>\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+# ----------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------
+# Info
+# ----------------------------------------------------------------------------------------------
+# https://gist.github.com/shanselman/25f5550ad186189e0e68916c6d7f44c3
+# https://github.com/PowerShell/PSReadLine/blob/master/PSReadLine/KeyBindings.cs
+# https://github.com/PowerShell/PSReadLine/blob/master/PSReadLine/KeyBindings.vi.cs
+# ----------------------------------------------------------------------------------------------
