db3ad37dc6a087fc5123d948b8c3e9b05468fc72
yavorski <yavorski.wrx@gmail.com>
2024-11-09 12:48:32 +0200

    bash

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,16 @@ function bootstrap() {
   rsync -avh --no-perms .vimrc .inputrc .gitconfig .shellcheckrc "$HOME"
 
   echo ""
-  rsync -avh --no-perms --delete bash nvim helix kitty alacritty starship/starship.toml "$HOME/.config"
+  rsync -avh --no-perms --delete bash fish "$HOME/.config"
 
   echo ""
-  rsync -avh --no-perms --delete sway waybar "$HOME/.config"
+  rsync -avh --no-perms --delete nvim helix kitty alacritty starship/starship.toml "$HOME/.config"
 
   echo ""
-  rsync -avh --no-perms --delete chrome/chrome-flags.conf chrome/chromium-flags.conf "$HOME/.config"
+  rsync -avh --no-perms --delete hypr sway swaync mpv nwg-bar fuzzel waybar "$HOME/.config"
+
+  echo ""
+  rsync -avh --no-perms --delete chrome/brave-flags.conf chrome/chrome-flags.conf chrome/chromium-flags.conf "$HOME/.config"
 
   # bash
   ln -sf "$HOME/.config/bash/.bashrc" "$HOME/.bashrc"
@@ -33,13 +36,11 @@ if ! command -v rsync &> /dev/null; then
 fi
 
 if [[ "$1" == "-f" || "$1" == "--force" ]]; then
-  backup
   bootstrap
 else
   read -r -p "This may overwrite existing files in home and home/.cofnig directories. Are you sure? (y/n) " -n 1
   echo ""
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    backup
     bootstrap
   fi
 fi
