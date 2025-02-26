ba165768b256eaa6776383b35826b92654fc555d
yavorski <yavorski.wrx@gmail.com>
2025-02-27 00:04:58 +0200

    nvim db

--- nvim/init.lua
+++ nvim/init.lua
@@ -1716,6 +1716,24 @@ Lazy.use {
 }
 
 ------------------------------------------------------------
+-- DB
+------------------------------------------------------------
+Lazy.use {
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod" },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" } }
+  },
+  cmd = { "DBUI", "DBUIToggle" },
+  keys = {{ [[<leader>\d]], "<cmd>DBUIToggle<cr>", silent = true, desc = "DBUI Toggle" }},
+  init = function()
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_use_nvim_notify = 1
+    -- vim.cmd[[autocmd FileType dbout setlocal nofoldenable foldmethod=manual]]
+  end
+}
+
+------------------------------------------------------------
 -- AutoComplete -- https://cmp.saghen.dev/
 ------------------------------------------------------------
 Lazy.use {
