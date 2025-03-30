003ae7868a48917424c6a261a45dca63be4208e4
yavorski <3436517+yavorski@users.noreply.github.com>
2025-03-31 01:22:19 +0300

    nvim diagnostics
    nvim v0.11

--- nvim/init.lua
+++ nvim/init.lua
@@ -199,6 +199,27 @@ vim.filetype.add({
 })
 
 ------------------------------------------------------------
+-- Diagnostics Config
+------------------------------------------------------------
+
+vim.diagnostic.config({
+  underline = false,
+  severity_sort = true,
+  update_in_insert = false,
+  virtual_lines = false,
+  virtual_text = { current_line = true },
+  float = { border = "rounded" },
+  signs = {
+    text = {
+      [ vim.diagnostic.severity.HINT ] = "★",
+      [ vim.diagnostic.severity.INFO ] = "▪",
+      [ vim.diagnostic.severity.WARN ] = "◮",
+      [ vim.diagnostic.severity.ERROR ] = "",
+    }
+  }
+})
+
+------------------------------------------------------------
 -- sysname
 ------------------------------------------------------------
 
