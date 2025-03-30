9b2fba1397605cbe5d932fe22a93268f5ded1c59
yavorski <3436517+yavorski@users.noreply.github.com>
2025-03-31 00:40:04 +0300

    nvim tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -831,13 +831,14 @@ Lazy.use {
   cmd = "NvimTreeToggle",
   opts = {
     git = { timeout = 2048 },
+    sync_root_with_cwd = true,
     update_focused_file = { enable = true },
   },
   keys = {
-    { [[<leader>\\]], "<cmd>NvimTreeToggle<cr>", silent = true, desc = "NvimTreeToggle" },
     { [[<leader>\r]], "<cmd>NvimTreeRefresh<cr>", silent = true, desc = "NvimTreeRefresh" },
     { [[<leader>\f]], "<cmd>NvimTreeFindFile<cr>", silent = true, desc = "NvimTreeFindFile" },
     { [[<leader>\F]], "<cmd>NvimTreeFindFile!<cr>", silent = true, desc = "NvimTreeFindFile!" },
+    { [[<leader>\\]], function() require("nvim-tree.api").tree.toggle({ focus = false }) end, silent = true, desc = "NvimTreeToggle" },
   }
 }
 
