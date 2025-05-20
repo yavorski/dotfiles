4707a42b509b34f1871c12a3356cd30ac6f0b12e
yavorski <3436517+yavorski@users.noreply.github.com>
2025-05-20 22:42:52 +0300

    nvim mini diff
    inline diff provider for codecompanion

--- nvim/init.lua
+++ nvim/init.lua
@@ -981,6 +981,20 @@ Lazy.use {
   end
 }
 
+-- inline diff provider for codecompanion
+Lazy.use {
+  "echasnovski/mini.diff",
+  config = function()
+    local MiniDiff = require("mini.diff")
+    MiniDiff.config.view.priority = -1
+    MiniDiff.config.delay.text_change = 10000
+    MiniDiff.config.source = MiniDiff.gen_source.none()
+    for key, _ in pairs(MiniDiff.config.mappings) do MiniDiff.config.mappings[key] = "" end
+    for key, _ in pairs(MiniDiff.config.view.signs) do MiniDiff.config.view.signs[key] = "" end
+    MiniDiff.setup()
+  end
+}
+
 -- show notifications
 Lazy.use {
   "echasnovski/mini.notify",
