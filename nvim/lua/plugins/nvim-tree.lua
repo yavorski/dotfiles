700a4e2cb7f12c9587b8a7a116b23e120f90c8ed
yavorski <3436517+yavorski@users.noreply.github.com>
2025-05-27 01:28:17 +0300

    nvim refactor
    Split neovim configuration into multiple files
    
    * lsp refactor
    * add lua types
    * fix lua lazydev
    * fix laststatus -> set 3
    * azure pipelines lsp
    * code companion mappings
    * nvim-tree popup info border
    * lualine fixes/updates - ts/lsp
    * run CatppuccinCompile on update
    * refactor & lazyload lsp-overloads
    * uptdate tree-sitter & treesitter-textobjects to main branch

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,28 @@
+--- @brief
+--- File explorer tree sidebar
+
+local Lazy = require("core/lazy")
+local window_border = require("core/border")
+
+Lazy.use {
+  "nvim-tree/nvim-tree.lua",
+  cmd = "NvimTreeToggle",
+  opts = {
+    git = { timeout = 2048 },
+    sync_root_with_cwd = true,
+    update_focused_file = { enable = true },
+    actions = {
+      file_popup = {
+        open_win_config = {
+          border = window_border
+        }
+      }
+    }
+  },
+  keys = {
+    { [[<leader>\r]], "<cmd>NvimTreeRefresh<cr>", silent = true, desc = "NvimTreeRefresh" },
+    { [[<leader>\f]], "<cmd>NvimTreeFindFile<cr>", silent = true, desc = "NvimTreeFindFile" },
+    { [[<leader>\F]], "<cmd>NvimTreeFindFile!<cr>", silent = true, desc = "NvimTreeFindFile!" },
+    { [[<leader>\\]], function() require("nvim-tree.api").tree.toggle({ focus = false }) end, silent = true, desc = "NvimTreeToggle" },
+  }
+}
