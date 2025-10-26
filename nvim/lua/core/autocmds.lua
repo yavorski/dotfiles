d23d62fde782ee56746c9a018289e94682a6901b
yavorski <3436517+yavorski@users.noreply.github.com>
2025-10-26 22:06:05 +0200

    nvim settings/autocmds/keymaps/diagnostics updates

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -29,11 +29,38 @@ vim.cmd[[autocmd TextYankPost * silent! lua vim.hl.on_yank({ higroup = "YankHigh
 -- 2 spaces for selected filetypes
 vim.cmd[[autocmd FileType xml,html,css,json,lua,yaml,markdown setlocal shiftwidth=2 tabstop=2 expandtab]]
 
--- fix htmlangular indentation with =
+-- fix indentation with =
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "htmlangular",
+  group = vim.api.nvim_create_augroup("local/indent", {}),
+  pattern = { "razor", "cshtml", "htmlangular" },
   callback = function()
     vim.bo.indentexpr = ""
     vim.cmd[[runtime! indent/html.vim]]
+  end
+})
+
+-- fix matchit in razor files
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("local/match", {}),
+  pattern = { "razor", "cshtml" },
+  callback = function()
+    -- Load matchit plugin first
+    if not vim.g.loaded_matchit then
+      vim.cmd[[runtime! plugin/matchit.vim]]
+    end
+
+    -- Load HTML ftplugin to get proper HTML tag matching
+    if not vim.b.did_ftplugin then
+      vim.cmd[[runtime! ftplugin/html.vim]]
+    end
+  end
+})
+
+-- stop on dashes when using "w"
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("local/styles", {}),
+  pattern = { "css", "less", "sass", "scss", "stylus" },
+  callback = function()
+    vim.opt_local.iskeyword:remove("-")
   end
 })
