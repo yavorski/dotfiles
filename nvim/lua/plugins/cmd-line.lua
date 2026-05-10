d168a68ec2d1f4e59013bea5fd348d679806b913
yavorski <3436517+yavorski@users.noreply.github.com>
2026-05-10 23:09:47 +0300

    nvim cmd-line

---
+++ nvim/lua/plugins/cmd-line.lua
@@ -0,0 +1,32 @@
+--- @brief Scratch Buffer Plugin
+--- Repositions the cmdline as a centered floating window powered ui2
+
+local Lazy = require("core/lazy")
+local border = require("core/border")
+
+Lazy.use {
+  src = "https://github.com/rachartier/tiny-cmdline.nvim",
+  event = "VeryLazy",
+  config = function()
+    --- @type TinyCmdlineConfig
+    local options = {
+      width = {
+        value = 86,
+        min = 86,
+        max = 96,
+      },
+
+      position = {
+        x = "50%",
+        y = "20%",
+      },
+
+      border = border,
+      menu_col_offset = 0,
+      native_types = { "/", "?" },
+      -- on_reposition = require("tiny-cmdline").adapters.blink,
+    }
+
+    require("tiny-cmdline").setup(options)
+  end
+}
