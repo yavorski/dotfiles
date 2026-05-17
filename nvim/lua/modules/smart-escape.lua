879f8e0a94854ed4a59457720c82a3f131cb17c5
yavorski <3436517+yavorski@users.noreply.github.com>
2026-05-17 22:32:19 +0300

    nvim refactor smart-escape

--- nvim/lua/modules/smart-escape.lua
+++ nvim/lua/modules/smart-escape.lua
@@ -1,25 +1,38 @@
 -- Map <esc>
--- Close all floating windows
--- Close quickfix list, location list, trouble
+-- Close floating windows, quickfix & location list
 
--- escape fn
+local KEEP_FILETYPES = {
+  minimap = true,
+  NvimTree = true,
+}
+
 local function escape()
-  -- vim.cmd("Noice dismiss")
   vim.cmd("silent! cclose | lclose")
 
   if vim.snippet.active() then
     return vim.snippet.stop()
   end
 
-  if vim.g.NvimTreeRequired == 1 then
-    vim.cmd("silent! 1fclose")
-  else
-    vim.cmd("silent! fclose!")
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    if vim.api.nvim_win_get_config(win).relative ~= "" then
+      local ft = vim.api.nvim_get_option_value("filetype", {
+        buf = vim.api.nvim_win_get_buf(win)
+      })
+
+      if not KEEP_FILETYPES[ft] then
+        pcall(vim.api.nvim_win_close, win, true)
+      end
+    end
   end
 
-  if MiniMap ~= nil then MiniMap.open() end
-  if MiniJump ~= nil and MiniJump.state.jumping then MiniJump.stop_jumping() end
+  if MiniMap ~= nil then
+    MiniMap.open()
+  end
+
+  if MiniJump ~= nil and MiniJump.state.jumping then
+    MiniJump.stop_jumping()
+  end
 end
 
 -- map <esc> key
-vim.keymap.set("n", "<esc>", escape, { silent = true, noremap = true, desc = "Escape" })
+vim.keymap.set("n", "<esc>", escape, { silent = true, desc = "Escape" })
