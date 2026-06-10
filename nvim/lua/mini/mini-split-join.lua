70ee71ff5175cba74d3611b3f9f9e8495d2a2dad
yavorski <3436517+yavorski@users.noreply.github.com>
2026-06-10 20:36:54 +0300

    nvim mini split-join pad/no-pad

--- nvim/lua/mini/mini-split-join.lua
+++ nvim/lua/mini/mini-split-join.lua
@@ -3,24 +3,43 @@
 
 local Lazy = require("core/lazy")
 
+-- join with padding brackets -> ( a, b, c )
+local function sj()
+  require("mini.splitjoin").toggle()
+end
+
+-- join without padding brackets -> (a, b, c)
+local function sJ()
+  require("mini.splitjoin").toggle({
+    join = {
+      hooks_post = {
+        require("mini.splitjoin").gen_hook.del_trailing_separator()
+      }
+    }
+  })
+end
+
+local function init_split_join()
+  local splitjoin = require("mini.splitjoin")
+
+  splitjoin.setup({
+    mappings = {
+      toggle = "sj"
+    },
+    join = {
+      hooks_post = {
+        splitjoin.gen_hook.pad_brackets(),
+        splitjoin.gen_hook.del_trailing_separator(),
+      }
+    }
+  })
+end
+
 Lazy.use {
   "nvim-mini/mini.splitjoin",
   keys = {
-    { "sj", function() require("mini.splitjoin").toggle() end, silent = true, desc = "Split/Join" }
+    { "sj", sj, silent = true, desc = "Split/Join" },
+    { "sJ", sJ, silent = true, desc = "Split/Join" }
   },
-  config = function()
-    local splitjoin = require("mini.splitjoin")
-
-    splitjoin.setup({
-      mappings = {
-        toggle = "sj"
-      },
-      join = {
-        hooks_post = {
-          splitjoin.gen_hook.pad_brackets(),
-          splitjoin.gen_hook.del_trailing_separator(),
-        }
-      }
-    })
-  end
+  config = init_split_join
 }
