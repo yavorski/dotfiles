bbc2104eb65159ede284c621722f86ded1c8ccba
yavorski <3436517+yavorski@users.noreply.github.com>
2026-09-02 21:26:08 +0300

    nvim simplify ts text objects

--- nvim/lua/plugins/tree-sitter-text-objects.lua
+++ nvim/lua/plugins/tree-sitter-text-objects.lua
@@ -8,15 +8,10 @@ Lazy.use {
   version = "main",
   dependencies = {{ "nvim-treesitter/nvim-treesitter", version = "main" }},
   keys = {
-    { "]c", desc = "@class next" },
-    { "[c", desc = "@class prev" },
-    { "]C", desc = "@class next end" },
-    { "[C", desc = "@class prev end" },
-
+    { "]k", desc = "@class next" },
+    { "[k", desc = "@class prev" },
     { "]f", desc = "@function next" },
     { "[f", desc = "@function prev" },
-    { "]F", desc = "@function next end" },
-    { "[F", desc = "@function prev end" },
   },
   config = function()
     require("nvim-treesitter-textobjects").setup({
@@ -28,11 +23,8 @@ Lazy.use {
     local move = require("nvim-treesitter-textobjects.move")
 
     --- classes
-    --- NOTE do not assign "]c" and "[c" in diff mode
-    vim.keymap.set({ "n", "x", "o" }, "]c", function() if vim.wo.diff then vim.cmd.normal({ "]c", bang = true }) else move.goto_next_start("@class.outer", "textobjects") end end, { desc = "@class next" })
-    vim.keymap.set({ "n", "x", "o" }, "[c", function() if vim.wo.diff then vim.cmd.normal({ "[c", bang = true }) else move.goto_previous_start("@class.outer", "textobjects") end end, { desc = "@class prev" })
-    vim.keymap.set({ "n", "x", "o" }, "]C", function() move.goto_next_end("@class.outer", "textobjects") end, { desc = "@class next end" })
-    vim.keymap.set({ "n", "x", "o" }, "[C", function() move.goto_previous_end("@class.outer", "textobjects") end, { desc = "@class prev end" })
+    vim.keymap.set({ "n", "x", "o" }, "]k", function() move.goto_next_start("@class.outer", "textobjects") end, { desc = "@class next" })
+    vim.keymap.set({ "n", "x", "o" }, "[k", function() move.goto_previous_start("@class.outer", "textobjects") end, { desc = "@class prev" })
 
     --- methods ]m [m ]M [M
     --- tree-sitter do not distinct between methods and functions
@@ -41,7 +33,5 @@ Lazy.use {
     --- functions/methods
     vim.keymap.set({ "n", "x", "o" }, "]f", function() move.goto_next_start("@function.outer", "textobjects") end, { desc = "@function next" })
     vim.keymap.set({ "n", "x", "o" }, "[f", function() move.goto_previous_start("@function.outer", "textobjects") end, { desc = "@function prev" })
-    vim.keymap.set({ "n", "x", "o" }, "]F", function() move.goto_next_end("@function.outer", "textobjects") end, { desc = "@function next end" })
-    vim.keymap.set({ "n", "x", "o" }, "[F", function() move.goto_previous_end("@function.outer", "textobjects") end, { desc = "@function prev end"})
   end
 }
