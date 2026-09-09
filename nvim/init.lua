fdb920e0a0593c055a6075993003770f5b794f67
yavorski <3436517+yavorski@users.noreply.github.com>
2026-09-09 20:57:02 +0300

    [WIP] nvim mc <C-d>

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,7 +36,14 @@ require("plugins/git-signs")
 require("plugins/stylus")
 require("plugins/auto-tag")
 require("plugins/code-diff")
-require("plugins/multi-cursor")
+
+if vim.fn.has("nvim-0.13") == 1 then
+  require("modules/multi-cursor")
+  vim.notify("nvim-13")
+else
+  require("plugins/multi-cursor")
+  vim.notify("nvim-12")
+end
 
 --- Tree-Sitter ---
 require("plugins/tree-sitter")
